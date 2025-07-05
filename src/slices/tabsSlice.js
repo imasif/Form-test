636bd247e4e2bf9f782f6a8fc9b049de68bae62d
asif <imasifkhan1010@gmail.com>
2025-07-06 00:29:26 +0600

    active tab is now being tracked with redux

--- src/slices/tabsSlice.js
+++ src/slices/tabsSlice.js
@@ -6,34 +6,44 @@ import detailsActive from '../assets/detailsActive.svg';
 import endingNormal from '../assets/endingNormal.svg';
 import endingActive from '../assets/endingActive.svg';
 
-const initialState = [
-  { id: 1, name: 'Info', icon_normal: infoIcon, icon_active: infoActiveIcon,
-  },
+const initialTabs = [
+  { id: 1, name: 'Info', icon_normal: infoIcon, icon_active: infoActiveIcon },
   { id: 2, name: 'Details', icon_normal: detailsNormal, icon_active: detailsActive },
   { id: 3, name: 'Other', icon_normal: detailsNormal, icon_active: detailsActive },
   { id: 4, name: 'Ending', icon_normal: endingNormal, icon_active: endingActive },
 ];
+
+const initialState = {
+  tabs: initialTabs,
+  activeTabId: initialTabs[0].id,
+};
 
 const tabsSlice = createSlice({
   name: 'tabs',
   initialState,
   reducers: {
     addTab: (state, action) => {
-      state.push(action.payload);
+      state.tabs.push(action.payload);
+      state.activeTabId = action.payload.id;
     },
     removeTab: (state, action) => {
-      return state.filter(tab => tab.id !== action.payload);
+      state.tabs = state.tabs.filter(tab => tab.id !== action.payload);
+      if (state.activeTabId === action.payload) {
+        state.activeTabId = state.tabs[0]?.id ?? null;
+      }
     },
     setTabs: (state, action) => {
-      return action.payload;
+      state.tabs = action.payload;
     },
     reorderTabs: (state, action) => {
-      // action.payload should be the new ordered array of tabs
-      return action.payload;
+      state.tabs = action.payload;
+    },
+    setActiveTab: (state, action) => {
+      state.activeTabId = action.payload;
     },
     resetTabs: () => initialState,
   },
 });
 
-export const { addTab, removeTab, setTabs, resetTabs, reorderTabs } = tabsSlice.actions;
+export const { addTab, removeTab, setTabs, resetTabs, reorderTabs, setActiveTab } = tabsSlice.actions;
 export default tabsSlice.reducer;
